ef777eb91bc2ab1d81ea5d0f888f606b9d23a502
asimon84 <ajsimon4@yahoo.com>
2025-11-18 21:03:57 -0800

    migration updates

--- database/migrations/2025_11_19_034028_create_payments_table.php
+++ database/migrations/2025_11_19_034028_create_payments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Invoice;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Invoice::class)->nullable()->nullOnDelete();
+            $table->float('amount', 2)->default(0.00);
             $table->timestamps();
         });
     }
