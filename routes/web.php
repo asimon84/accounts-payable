c293dd8fe5e2153ee1cd2d7767bfbdf0369348a5
asimon84 <ajsimon4@yahoo.com>
2025-11-19 18:43:23 -0800

    route fix

--- routes/web.php
+++ routes/web.php
@@ -5,19 +5,15 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-//Route::get('/', function () {
-//    return Inertia::render('welcome', [
-//        'canRegister' => Features::enabled(Features::registration()),
-//    ]);
-//})->name('home');
-//
-//Route::middleware(['auth', 'verified'])->group(function () {
-//    Route::get('dashboard', function () {
-//        return Inertia::render('dashboard');
-//    })->name('dashboard');
-//});
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
 
-Route::get('/', [DashboardController::class, 'index'])->name('home');
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('home');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 require __DIR__.'/settings.php';
