4d330c4126047ed0eebbf17b96282f57438f42b0
asimon84 <ajsimon4@yahoo.com>
2025-11-19 18:13:05 -0800

    Dashboard Controller update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use App\Models\Payment;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
-    //
+    /**
+     * Show the view for the Dashboard page
+     *
+     * @param Request $request
+     *
+     * @return View
+     */
+    public function index(Request $request):View
+    {
+        $invocies = Invoice::all();
+        $payments = Payment::all();
+
+        return view('dashboard', compact('invocies', 'payments'));
+    }
 }
