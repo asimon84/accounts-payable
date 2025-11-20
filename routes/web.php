933a54170f1af5784d3eb93f70e11738517952a0
asimon84 <ajsimon4@yahoo.com>
2025-11-19 18:14:23 -0800

    route update

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,23 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
-Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
-})->name('home');
+//Route::get('/', function () {
+//    return Inertia::render('welcome', [
+//        'canRegister' => Features::enabled(Features::registration()),
+//    ]);
+//})->name('home');
+//
+//Route::middleware(['auth', 'verified'])->group(function () {
+//    Route::get('dashboard', function () {
+//        return Inertia::render('dashboard');
+//    })->name('dashboard');
+//});
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+Route::get('/', [DashboardController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 require __DIR__.'/settings.php';
