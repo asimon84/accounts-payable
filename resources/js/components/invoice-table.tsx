90da50288f418fa0c91f506dffe6144537c822a4
asimon84 <ajsimon4@yahoo.com>
2025-11-20 22:39:55 -0800

    axios api client updates

--- resources/js/components/invoice-table.tsx
+++ resources/js/components/invoice-table.tsx
@@ -3,27 +3,19 @@ import React, { useEffect, useState } from 'react';
 import DataTable from 'datatables.net-react';
 import 'datatables.net-dt';
 import axios from 'axios';
+import apiClient from '@/components/api.tsx';
 
 export function InvoiceTable() {
     const [invoices, setInvoices] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    axios.defaults.withCredentials = true;
-
-    async function getCsrfToken() {
-        try {
-            await axios.get('/sanctum/csrf-cookie');
-            console.log('token='+axios.get('/sanctum/csrf-cookie'));
-        } catch (error) {
-            console.error('Error fetching CSRF cookie:', error);
-        }
-    }
-
     useEffect(() => {
         const fetchInvoices = async () => {
             try {
-                getCsrfToken();
-                const response = await axios.get('/api/invoices');
+                // const response = await axios.get('/api/invoices');
+                const response = apiClient.get('/invoices').then(
+                    console.log('test')
+                );
                 setInvoices(response.data.data); // Assuming paginated data is in 'data' key
                 setLoading(false);
             } catch (error) {
