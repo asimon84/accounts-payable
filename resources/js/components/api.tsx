ee967febdbf9cd8ee2272268c197b15954e360ae
asimon84 <ajsimon4@yahoo.com>
2025-11-21 13:49:02 -0800

    api auth updates

--- resources/js/components/api.tsx
+++ resources/js/components/api.tsx
@@ -1,10 +1,17 @@
 import axios from 'axios';
 
+const token = localStorage.getItem('authToken'); // Or retrieve from your state
+
+if (token) {
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
 const apiClient = axios.create({
     baseURL: '/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
     },
 });
 
