ce1da074e64869a5e5f681fa7499ef2254e0aa66
asimon84 <ajsimon4@yahoo.com>
2025-11-23 15:51:12 -0800

    create page updated

--- resources/js/pages/create.tsx
+++ resources/js/pages/create.tsx
@@ -3,7 +3,6 @@ import AppLayout from '@/layouts/app-layout';
 import { create } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-import { InvoiceTable } from '@/components/invoice-table';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,13 +11,13 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function Dashboard() {
+export default function Create() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <InvoiceTable/>
+
                 </div>
             </div>
         </AppLayout>
