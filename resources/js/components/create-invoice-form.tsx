f655c2edc2cd760ea41e17fa7e58a8b59b5db1b3
asimon84 <ajsimon4@yahoo.com>
2025-11-24 23:05:31 -0800

    create invoice form updates

---
+++ resources/js/components/create-invoice-form.tsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import apiClient from '@/components/api.tsx';
+
+export function CreateInvoiceForm() {
+
+}
--- resources/js/pages/create.tsx
+++ resources/js/pages/create.tsx
@@ -3,6 +3,7 @@ import AppLayout from '@/layouts/app-layout';
 import { create } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { CreateInvoiceForm } from '@/components/create-invoice-form.tsx';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -17,7 +18,7 @@ export default function Create() {
             <Head title="Create Invoice" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-
+                    <CreateInvoiceForm/>
                 </div>
             </div>
         </AppLayout>
