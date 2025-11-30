68c19631e0f351e929caa5ecf0b2e75f5544bc92
asimon84 <ajsimon4@yahoo.com>
2025-11-29 20:23:22 -0800

    routes clean up

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ Route::post('/token/create', [AuthController::class, 'createToken'])->name('toke
 
 ////Routes Behind Authentication
 //Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/invoices', [InvoiceController::class, 'index'])->name('invoice.index');
     Route::post('/invoice', [InvoiceController::class, 'store'])->name('invoice.store');
     Route::get('/invoice/{invoice}', [InvoiceController::class, 'show'])->name('invoice.show');
     Route::put('/invoice/{invoice}', [InvoiceController::class, 'edit'])->name('invoice.update');
