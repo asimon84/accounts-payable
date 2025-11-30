c5c0b5ee6da647d64dd7da924753bc3feb8944e6
asimon84 <ajsimon4@yahoo.com>
2025-11-29 19:34:55 -0800

    details test added

---
+++ tests/Feature/DetailsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Invoice;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DetailsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_guests_are_redirected_to_the_login_page()
+    {
+        $invoice = Invoice::factory()->create();
+
+        $this->get('/invoice/'.$invoice->id)->assertRedirect(route('login'));
+    }
+
+    public function test_authenticated_users_can_visit_details_page()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $invoice = Invoice::factory()->create();
+
+        $this->get('/invoice/'.$invoice->id)->assertOk();
+    }
+}
