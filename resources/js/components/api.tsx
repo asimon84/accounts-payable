871213065a5d6b442bb2beef8d1d68c0693855da
asimon84 <ajsimon4@yahoo.com>
2025-12-04 20:29:57 -0800

    api updates

--- resources/js/components/api.tsx
+++ resources/js/components/api.tsx
@@ -1,19 +1,35 @@
 import axios from 'axios';
 
-// const token = localStorage.getItem('authToken'); // Or retrieve from your state
-//
-// if (token) {
-//     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
-// }
+const token = getToken();
 
-// async function getCsrfCookie() {
-//     try {
-//         await axios.get('/sanctum/csrf-cookie', { withCredentials: true });
-//         console.log('CSRF cookie obtained successfully.');
-//     } catch (error) {
-//         console.error('Error obtaining CSRF cookie:', error);
-//     }
-// }
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+async function getToken() {
+    try {
+        const email = 'test@example.com';
+        const password = 'test1234';
+        const response = await axios.post(`/api/login`, { email, password });
+
+        localStorage.setItem('access_token', response.data.token);
+        console.log(response);
+
+        return response.data.token;
+    } catch (error) {
+        console.error('Error obtaining token:', error);
+    }
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+async function getCsrfCookie() {
+    try {
+        await axios.get('/sanctum/csrf-cookie', { withCredentials: true });
+
+        console.log('CSRF cookie obtained successfully.');
+    } catch (error) {
+        console.error('Error obtaining CSRF cookie:', error);
+    }
+}
 
 const apiClient = axios.create({
     baseURL: '/api',
@@ -22,7 +38,7 @@ const apiClient = axios.create({
         'X-CSRF-TOKEN': document.querySelector('input[name="_token"]').value,
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        // 'Authorization': `Bearer ${token}`,
+        'Authorization': `Bearer ${token}`,
     },
 });
 
