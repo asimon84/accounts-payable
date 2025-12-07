a4401326ad8e3576c8bb9331261b1f8869394871
asimon84 <ajsimon4@yahoo.com>
2025-12-06 23:11:36 -0800

    styling updates

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -17,7 +17,7 @@ export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
-            <div id="dashboard-container"  className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <div id="dashboard-container" className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <ReportSummary/>
             </div>
         </AppLayout>
--- resources/js/pages/invoices.tsx
+++ resources/js/pages/invoices.tsx
@@ -4,6 +4,7 @@ import { invoices } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import { InvoiceTable } from '@/components/invoice-table';
+import '../../css/invoices.css';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,7 +17,7 @@ export default function Invoice() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Invoices" />
-            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+            <div id="invoices-container" className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
                 <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
                     <InvoiceTable/>
                 </div>
