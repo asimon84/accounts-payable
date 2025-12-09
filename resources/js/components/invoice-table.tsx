67e3478d15d2e26c567861c05ab7d7461552d322
asimon84 <ajsimon4@yahoo.com>
2025-12-08 22:05:38 -0800

    code clean up

--- resources/js/components/invoice-table.tsx
+++ resources/js/components/invoice-table.tsx
@@ -13,19 +13,10 @@ DataTable.use(DT);
 DT.Buttons.jszip(jszip);
 DT.Buttons.pdfMake(pdfmake);
 
-// export function InvoiceTable({ totalRows, onPageChange, onRowsPerPageChange }) {
 export function InvoiceTable() {
     const [invoices, setInvoices] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
-    // const customPaginationOptions = {
-    //     rowsPerPageText: 'Records per page:',
-    //     rangeSeparatorText: 'of',
-    //     noRowsPerPage: false,
-    //     selectAllRowsItem: false,
-    //     selectAllRowsItemText: 'All',
-    // };
 
     useEffect(() => {
         const fetchInvoices = async () => {
@@ -42,13 +33,6 @@ export function InvoiceTable() {
         fetchInvoices();
     }, []);
 
-    // const columns = [
-    //     { name: 'ID', selector: row => row.id, sortable: true },
-    //     { name: 'Name', selector: row => row.customer_name, sortable: true },
-    //     { name: 'Due', selector: row => row.due_date, sortable: true },
-    //     { name: 'Paid', selector: row => row.paid, sortable: true },
-    // ];
-
     const columns = [
         { data: 'id' },
         { data: 'customer_name' },
@@ -59,32 +43,6 @@ export function InvoiceTable() {
     if (loading) return <div id="invoice-table-loading">Loading Invoices...</div>;
 
     if (error) return <p id="invoice-table-error">Error: {error.message}</p>;
-
-    // const handleRowClick = (row) => {
-    //     window.location.href = '/invoice/'+row.id;
-    // };
-    //
-    // return (
-    //     <div id="invoice-table-container" className="relative flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-    //         <DataTable
-    //             columns={columns}
-    //             data={invoices}
-    //             options={{
-    //                     paging: true,
-    //                     searching: true,
-    //                 }}
-    //             onRowClicked={handleRowClick}
-    //             pagination
-    //             paginationPerPage={10}
-    //             paginationRowsPerPageOptions={[5, 10, 20, 50]}
-    //             paginationTotalRows={totalRows}
-    //             onChangePage={onPageChange}
-    //             onChangeRowsPerPage={onRowsPerPageChange}
-    //             paginationComponentOptions={customPaginationOptions}
-    //             highlightOnHover
-    //         />
-    //     </div>
-    // );
 
     return (
         <DataTable
