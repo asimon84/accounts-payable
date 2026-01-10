df5c013cb493f1dd4e78538cf9b7bee4c6eaf6fd
asimon84 <ajsimon4@yahoo.com>
2026-01-09 21:43:24 -0800

    Edit Item test renamed

--- tests/Feature/EditItemTest.php
+++ tests/Feature/EditItemTest.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ItemTest extends TestCase
+class EditItemTest extends TestCase
 {
     use RefreshDatabase;
 
