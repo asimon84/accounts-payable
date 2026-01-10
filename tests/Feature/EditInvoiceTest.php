81fe7f67c6584b8ae2c3fa8f2bb03397abb139f4
asimon84 <ajsimon4@yahoo.com>
2026-01-09 21:21:46 -0800

    edit invoice test

--- tests/Feature/EditInvoiceTest.php
+++ tests/Feature/EditInvoiceTest.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class InvoiceTest extends TestCase
+class EditInvoiceTest extends TestCase
 {
     use RefreshDatabase;
 
