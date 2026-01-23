ee46f626366693d7c603842e97ca65365e3b24c4
asimon84 <ajsimon4@yahoo.com>
2026-01-22 21:26:19 -0800

    factory updates

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -16,10 +16,17 @@ class InvoiceFactory extends Factory
      */
     public function definition(): array
     {
+        $days_ago = rand(0, 7);
+
+        $date = new \DateTime();
+
+        $date->modify("-$days_ago days");
+
         return [
             'customer_name' => fake()->firstName() . fake()->lastName(),
             'due_date' => fake()->date(),
             'paid' => fake()->boolean(),
+            'created_at' => $date->format('Y-m-d H:i:s'),
         ];
     }
 }
