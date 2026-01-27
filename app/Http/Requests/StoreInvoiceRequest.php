a3314ff81ad6d86e502507410a5a8669917bcc74
asimon84 <ajsimon4@yahoo.com>
2026-01-26 22:05:04 -0800

    invoice requests updated

--- app/Http/Requests/StoreInvoiceRequest.php
+++ app/Http/Requests/StoreInvoiceRequest.php
@@ -11,7 +11,7 @@ class StoreInvoiceRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreInvoiceRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'customer_name' => 'string|max:255',
+            'due_date' => 'string|max:255',
+            'paid' => 'boolean',
         ];
     }
 }
